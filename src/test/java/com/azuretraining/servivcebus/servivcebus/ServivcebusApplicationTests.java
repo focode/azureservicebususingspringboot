93b2a380f05be98197448754aac3de0361b3c5a8
edn3vs <edn3vs@pl1inpun0383nb.net.plm.eds.com>
2017-07-26 14:24:05 +0530

    spring boot example to communicate with Azure Service bus

---
+++ src/test/java/com/azuretraining/servivcebus/servivcebus/ServivcebusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azuretraining.servivcebus.servivcebus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServivcebusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
